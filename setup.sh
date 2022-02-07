a76ce33dbd6fb3a9ab73d521ce933e999cd90b8d
xtianxian <46081072+xtianxian@users.noreply.github.com>
2022-02-08 06:13:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt update
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt install stunnel -y
